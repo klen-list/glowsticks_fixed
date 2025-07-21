908ac1223ed7598dd9a48b1da80c558ffdf89caa
Klen_list <klenlist@pika-soft.ru>
2025-07-21 11:16:46 +0300

    Add wsid for client downloading

--- lua/glowsticks/server.lua
+++ lua/glowsticks/server.lua
@@ -1,5 +1,4 @@
--- TODO: add workshop id for this addon
---resource.AddWorkshop(id)
+resource.AddWorkshop('3530948818')
 
 CreateConVar('gmod_glowsticks_lifetime_infinite', '0', FCVAR_NOTIFY, 'Sets glow stick\'s lifetime to infinite')
 CreateConVar('gmod_glowsticks_lifetime', '25', FCVAR_NOTIFY, 'Sets glow stick\'s lifetime in seconds')
