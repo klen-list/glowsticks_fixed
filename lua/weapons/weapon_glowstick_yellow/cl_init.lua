6bd8fe76b339d631c7e126fcc02d2136c874580b
Klen_list <klenlist@pika-soft.ru>
2025-07-21 10:57:27 +0300

    Old version of glowstick (to see diff)

---
+++ lua/weapons/weapon_glowstick_yellow/cl_init.lua
@@ -0,0 +1,7 @@
+include('shared.lua')
+
+SWEP.Slot			= 1; 
+SWEP.SlotPos		= 1; 
+SWEP.Spawnable		= true
+SWEP.AdminSpawnable	= true
+SWEP.WepSelectIcon	= surface.GetTextureID( "vgui/entities/glowstick" )
